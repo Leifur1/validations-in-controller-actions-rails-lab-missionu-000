1ac721f157fc12355dd617ec80c09d6a25cc81f9
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-18 16:51:51 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates :name, presence: true
+  validates :title, presence: true
   validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
   validates :content, length: { maximum: 100 }
 end
